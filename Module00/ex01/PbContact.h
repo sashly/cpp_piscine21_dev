b4bde71ff59305a9e0b87675a27247eb7fbfe86e
sashly <sashly@student.21-school.ru>
2021-07-25 11:36:47 +0300

    Module00 ex01 initials

---
+++ Module00/ex01/PbContact.h
@@ -0,0 +1,20 @@
+#ifndef PB_CONTACT_H
+# define PB_CONTACT_H
+
+# include <string>
+
+class PbContact {
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+public:
+	PbContact() {
+
+	}
+
+};
+
+
+#endif
---
+++ Module00/ex01/PhoneBook.h
@@ -0,0 +1,11 @@
+#ifndef PHONE_BOOK_H
+# define PHONE_BOOK_H
+
+# include "PbContact.h"
+
+class PhoneBook {
+	PbContact	contacts[8];
+
+};
+
+#endif
